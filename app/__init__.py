67cde42d764b3c5fc9f24b805dccb230b60626e6
ciccone1978 <ciccone1978@gmail.com>
2022-06-24 15:35:07 +0200

    bootstrap css

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from ensurepip import bootstrap
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -6,6 +7,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
